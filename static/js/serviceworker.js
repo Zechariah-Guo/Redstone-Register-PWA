46bf55018e91a2babd8cae29717d17df7a4366c4
Zechariah <zechariah.guo@education.nsw.gov.au>
2026-04-02 18:27:25 +1100

    Small edge case offline mode fix

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,10 +1,11 @@
-const OFFLINE_URL = "/offline.html";
+const OFFLINE_URL = "/offline";
 
-const assets = [
+const CORE_ASSETS = [
     "/",
     "/catalogue",
     "/about",
     OFFLINE_URL,
+    "/offline.html",
     "/static/css/style.css",
     "/static/css/theme-variables.css",
     "/static/js/app.js",
@@ -15,28 +16,37 @@ const assets = [
     "/static/fonts/kJF1BvYX7BgnkSrUwT8OhrdQw4oELdPIeeII9v6oDMzByHX9rA6RzaxHMPdY43zj-jCxv3fzvRNU22ZXGJpEpjC_1v-p_4MrImHCIJIZrDCvHOej.woff2",
     "/static/images/logo.webp",
     "/static/images/favicon.webp",
+    "/static/images/logo-footer.webp",
+    "/static/icons/icon-128x128.png",
+    "/static/icons/icon-192x192.png",
+    "/static/icons/icon-384x384.png",
+    "/static/icons/icon-512x512.png",
+];
+
+const OPTIONAL_ASSETS = [
     "/static/images/Burger-icon.webp",
     "/static/images/Basic%20Redstone.webp",
     "/static/images/Simple-Redstone.avif",
     "/static/images/Logical-Redstone.webp",
     "/static/images/Wiki.webp",
-    "/static/icons/icon-128x128.png",
-    "/static/icons/icon-192x192.png",
-    "/static/icons/icon-384x384.png",
-    "/static/icons/icon-512x512.png",
+    "/static/images/Falling_Lucky_Blocks_Thumbnail_0.webp",
     "/static/icons/desktop_screenshot.png",
     "/static/icons/mobile_screenshot.png",
-    "/static/images/logo-footer.webp",
 ];
 
-const CATALOGUE_ASSETS = "catalogue-assets-v9";
+const CATALOGUE_ASSETS = "catalogue-assets-v10";
 
 self.addEventListener("install", (installEvt) => {
     installEvt.waitUntil(
         caches
             .open(CATALOGUE_ASSETS)
             .then((cache) => {
-                return cache.addAll(assets);
+                return cache.addAll(CORE_ASSETS).then(() => {
+                    const optionalRequests = OPTIONAL_ASSETS.map((asset) => {
+                        return cache.add(asset);
+                    });
+                    return Promise.allSettled(optionalRequests);
+                });
             })
             .then(() => self.skipWaiting())
             .catch((error) => {
@@ -68,10 +78,19 @@ self.addEventListener("fetch", function (evt) {
         return;
     }
 
-    if (evt.request.mode === "navigate") {
+    const acceptsHeader = evt.request.headers.get("accept") || "";
+    const isHtmlRequest =
+        evt.request.mode === "navigate" ||
+        evt.request.destination === "document" ||
+        acceptsHeader.includes("text/html");
+
+    if (isHtmlRequest) {
         evt.respondWith(
             fetch(evt.request)
                 .then((response) => {
+                    if (!response || !response.ok) {
+                        throw new Error("Network response not ok");
+                    }
                     const responseCopy = response.clone();
                     caches.open(CATALOGUE_ASSETS).then((cache) => {
                         cache.put(evt.request, responseCopy);
@@ -79,9 +98,11 @@ self.addEventListener("fetch", function (evt) {
                     return response;
                 })
                 .catch(() => {
-                    return caches.match(evt.request).then((cachedResponse) => {
-                        return cachedResponse || caches.match(OFFLINE_URL);
-                    });
+                    return caches
+                        .match(evt.request, { ignoreSearch: true })
+                        .then((cachedResponse) => {
+                            return cachedResponse || caches.match(OFFLINE_URL);
+                        });
                 })
         );
         return;
